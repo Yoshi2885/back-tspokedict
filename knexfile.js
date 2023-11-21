93eccbf6f82fdc8284707ce4fdf48ca8c6f099d0
Yoshi2885 <yoshitaka_tanaka_aa@mail.toyota.co.jp>
2023-11-21 17:07:26 +0900

    knexfile change

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,7 @@ module.exports = {
       host: process.env.DB_HOST || "127.0.0.1",
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_NAME,
-      user: process.env.DB_USER,
+      user: process.env.DB_USER || "user",
       password: process.env.DB_PASSWORD,
     },
   },
